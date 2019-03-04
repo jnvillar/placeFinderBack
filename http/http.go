9429417e725837acf017469a7b2ed8104498e2ce
juany360 <juaninv@outlook.com>
2019-03-04 18:30:57 -0300

    disable cors

--- http/http.go
+++ http/http.go
@@ -16,7 +16,7 @@ import (
 func RegisterHttpHandlers(f *app.Factory){
 	graphqlHandler := http.HandlerFunc(graphqlHandlerFunc)
 	http.HandleFunc("/", graphiql.ServeGraphiQL)
-	http.Handle("/graphql",addContext(graphqlHandler,f))
+	http.Handle("/graphql",disableCors(addContext(graphqlHandler,f)))
 }
 
 func StartServer(port string){
@@ -49,6 +49,24 @@ func graphqlHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	buff, _ = json.Marshal(result)
 	w.Write(buff)
+}
+
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+
+		// I added this for another handler of mine,
+		// but I do not think this is necessary for GraphQL's handler
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
 
 func addContext(next http.Handler, f *app.Factory) http.Handler {
